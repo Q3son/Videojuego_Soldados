7f9e44ef20ce2897c4939c79e6172c28c750f608
Q3son <esubiahu@unsa.edu.pe>
2024-10-18 12:17:26 -0500

    Esta es la primera versión de mi clase Soldado:
    - Se crearon los atributos: Nombre, NivelVida, Fila y Columna, para luego ser usados en el metodo principal main

--- VideoJuego_Soldados/Soldado.java
+++ VideoJuego_Soldados/Soldado.java
@@ -1,3 +1,36 @@
-public class Soldado {
+//BY: SUBIA_EDSON_FP2
 
+public class Soldado {
+    private String nombre;
+    private int nivelVida;
+    private int fila;
+    private int columna;
+
+    public Soldado(String nombre, int nivelVida, int fila, int columna) {
+        this.nombre = nombre;
+        this.nivelVida = nivelVida;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivelVida() {
+        return nivelVida;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Nivel de vida: " + nivelVida + ", Posición: (" + fila + ", " + columna + ")";
+    }
 }
